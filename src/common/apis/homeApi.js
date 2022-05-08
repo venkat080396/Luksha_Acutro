eea8326d45b5b8c95556186699771766636a1445
prashantpanaskar <50726888+prashantpanaskar@users.noreply.github.com>
2022-05-08 22:27:54 +0530

    Updated API URL

--- src/common/apis/homeApi.js
+++ src/common/apis/homeApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://mhgim8mpk1.execute-api.eu-west-1.amazonaws.com/default/AcutroAPI",
+    baseURL: "https://sh6y4bl527.execute-api.eu-west-1.amazonaws.com/default/AcutroAPI",
     headers: {
         'Content-Type': 'application/json'
     }
