0484cbbbcd7341e24fc350b3e3879943075403b8
prashantpanaskar <50726888+prashantpanaskar@users.noreply.github.com>
2022-05-08 22:19:20 +0530

    Initial commit

---
+++ src/components/forms/TextField/TextField.js
@@ -0,0 +1,24 @@
+import React from "react";
+import MuiTextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import "./TextField.Styles.css";
+
+const TextField = ({ iconStart, iconEnd, ...props }) => {
+    return (
+        <MuiTextField
+            className="textfield"
+            placeholder={props.name}
+            {...props}
+            InputProps={{
+                startAdornment: iconStart ? (
+                    <InputAdornment position="start">{iconStart}</InputAdornment>
+                ) : null,
+                endAdornment: iconEnd ? (
+                    <InputAdornment position="end">{iconEnd}</InputAdornment>
+                ) : null
+            }}
+        />
+    );
+};
+
+export default TextField;
