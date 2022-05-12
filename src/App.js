9caeb92c84380afe3dbc00c37c8ffadd7e268362
Prashant Panaskar <prash.panaskar@gmail.com>
2022-05-12 19:15:40 +0530

    Filter functionality changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Login from './pages/Login/Login';
 import Alerts from './pages/Alerts/Alerts';
 import BuildingData from './pages/BuildingData/BuildingData';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Utilities from './pages/Reports/Utilities/Utilities'
+import HVACEfficiency from './pages/Reports/HVACEfficiency/HVACEfficiency'
+import Comfort from './pages/Reports/Comfort/Comfort'
+import Exports from './pages/Reports/Exports/Exports'
+
 //const LoginContainer = React.lazy(() => import("./pages/Login/Container"));
 //const Dashboard = React.lazy(() => import("./pages/Dashboard/Dashboard"));
 
@@ -16,6 +21,10 @@ function App() {
       <Route path="dashboard" element={<Dashboard />} />
       <Route path="alerts" element={<Alerts />} />
       <Route path="buildingData" element={<BuildingData />} />
+      <Route path="utilities" element={<Utilities />} />
+      <Route path="HVACEfficiency" element={<HVACEfficiency />} />
+      <Route path="comfort" element={<Comfort />} />
+      <Route path="exports" element={<Exports />} />
     </Routes>
   );
 }
